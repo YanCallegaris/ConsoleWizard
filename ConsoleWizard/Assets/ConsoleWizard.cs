cf109a6bad397f3ac6d42c0164689d8d889fdc4c
YanCallegaris <callegaris.yan@gmail.com>
2021-12-10 21:00:49 -0300

    Inicio do Console Wizard

--- ConsoleWizard/Assets/ConsoleWizard.cs
+++ ConsoleWizard/Assets/ConsoleWizard.cs
@@ -7,7 +7,12 @@ public class ConsoleWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("teste");
+        int max = 1000;
+        int min = 1;
+
+        Debug.Log("Welcome to number wizard\nPick a number and don't tell me what is\nThe highest number you can pick is: " 
+            + max + "\nThe lowest is: " + min + "\nTell me if your number is higher or lower than 500\nPush Up = Higher, Push Down = Lower, Push Enter = correct");
+
     }
 
     // Update is called once per frame
